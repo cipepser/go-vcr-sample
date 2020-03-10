a2a69c47ece9d913ce8c3244645a5dfab4b72de5
cipepser <respepic@gmail.com>
2020-03-10 14:06:46 +0900

    omit unsed fields of User

--- qiita/client.go
+++ qiita/client.go
@@ -17,22 +17,9 @@ func NewClient() *Client {
 }
 
 type User struct {
-	Description       string `json:"description"`
-	FacebookID        string `json:"facebook_id"`
-	FolloweesCount    int    `json:"followees_count"`
-	FollowersCount    int    `json:"followers_count"`
-	GithubLoginName   string `json:"github_login_name"`
+	// omit unused fields.
 	ID                string `json:"id"`
-	ItemsCount        int    `json:"items_count"`
-	LinkedinID        string `json:"linkedin_id"`
 	Location          string `json:"location"`
-	Name              string `json:"name"`
-	Organization      string `json:"organization"`
-	PermanentID       int    `json:"permanent_id"`
-	ProfileImageURL   string `json:"profile_image_url"`
-	TeamOnly          bool   `json:"team_only"`
-	TwitterScreenName string `json:"twitter_screen_name"`
-	WebsiteURL        string `json:"website_url"`
 }
 
 func (c *Client) FetchUser(id string) (*User, error) {
